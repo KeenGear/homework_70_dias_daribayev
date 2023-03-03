b82f03476dea57b55db5d08ac79a78c5bf01d5ec
Dias Daribayev <fission.frames-0s@icloud.com>
2023-03-03 13:29:51 +0600

    Added Django widgets to forms.py and removed unneeded anymore addition tags form from task_form.html

--- todoapp/forms.py
+++ todoapp/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Task, Tag
+from .models import Task
 
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['summary', 'description', 'status', 'tags']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+            'tags': forms.SelectMultiple(attrs={'class': 'form-control'}),
+        }
