c0813d3f4446cdca5f9dd14f857178b428d58ce6
Dias Daribayev <fission.frames-0s@icloud.com>
2023-03-07 09:18:58 +0600

    Added validations to Summary and Description, updated task_form.html as well

--- todoapp/forms.py
+++ todoapp/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import Task
 
 
@@ -12,3 +14,15 @@ class TaskForm(forms.ModelForm):
             'status': forms.Select(attrs={'class': 'form-control'}),
             'tags': forms.SelectMultiple(attrs={'class': 'form-control'}),
         }
+
+    def clean_summary(self):
+        summary = self.cleaned_data.get('summary')
+        if len(summary) < 5:
+            raise ValidationError("Summary must be at least 5 characters long.")
+        return summary
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        if len(description) < 10:
+            raise ValidationError("Description must be at least 10 characters long.")
+        return description
