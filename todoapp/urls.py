1748040622055e06e870bee8b3efbd7ff3bed59d
Dias Daribayev <fission.frames-0s@icloud.com>
2023-03-10 13:48:58 +0600

    Added views and urls

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import TaskCreateView, TaskUpdateView, TaskDeleteView, TaskListView, TaskDeleteSelectedView, TaskDetailView
+from .views import TaskCreateView, TaskUpdateView, TaskDeleteView, TaskListView, TaskDeleteSelectedView, TaskDetailView, \
+    ProjectListView, ProjectCreateView, ProjectDeleteView, ProjectUpdateView, ProjectDeleteSelectedView, ProjectDetailView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task_list'),
@@ -8,4 +9,10 @@ urlpatterns = [
     path('tasks/<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),
     path('tasks/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
     path('tasks/delete_selected/', TaskDeleteSelectedView.as_view(), name='task_delete_selected'),
+    path('projects/', ProjectListView.as_view(), name='project_list'),
+    path('<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
+    path('projects/create/', ProjectCreateView.as_view(), name='project_create'),
+    path('projects/<int:pk>/update/', ProjectUpdateView.as_view(), name='project_update'),
+    path('projects/<int:pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
+    path('projects/delete_selected/', ProjectDeleteSelectedView.as_view(), name='project_delete_selected'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,75 +1,53 @@
+from django.core.exceptions import ValidationError
+from django.db.models import Q
 from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import TemplateView
-from .models import Task
-from .forms import TaskForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Task, Project
+from .forms import TaskForm, ProjectForm
 
 
-class TaskListView(TemplateView):
+class TaskListView(ListView):
     template_name = 'task_list.html'
+    model = Task
+    context_object_name = 'tasks'
+    ordering = 'updated_at'
+    paginate_by = 5
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tasks'] = Task.objects.all()
-        return context
 
-
-class TaskDetailView(TemplateView):
+class TaskDetailView(DetailView):
     template_name = 'task_view.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['task'] = Task.objects.get(pk=self.kwargs['pk'])
-        return context
+    model = Task
+    context_object_name = 'task'
 
 
-class TaskCreateView(TemplateView):
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
     template_name = 'task_form.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = TaskForm()
-        return context
-
-    def post(self, request, *args, **kwargs):
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            task = form.save()
-            return redirect('task_detail', pk=task.pk)
-        else:
-            context = self.get_context_data(**kwargs)
-            context['form'] = form
-            return self.render_to_response(context)
+    success_url = reverse_lazy('task_list')
 
 
-class TaskUpdateView(TemplateView):
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
     template_name = 'task_form.html'
-
-    def get(self, request, pk):
-        task = get_object_or_404(Task, pk=pk)
-        form = TaskForm(instance=task)
-        return self.render_to_response({'form': form})
-
-    def post(self, request, pk):
-        task = get_object_or_404(Task, pk=pk)
-        form = TaskForm(request.POST, instance=task)
-        if form.is_valid():
-            task = form.save()
-            return redirect('task_detail', pk=task.pk)
-        return self.render_to_response({'form': form})
+    context_object_name = 'task'
+    success_url = reverse_lazy('task_list')
 
 
-class TaskDeleteView(TemplateView):
+class TaskDeleteView(DeleteView):
+    model = Task
     template_name = 'task_confirm_delete.html'
+    success_url = reverse_lazy('task_list')
 
-    def get(self, request, pk):
-        task = get_object_or_404(Task, pk=pk)
-        return self.render_to_response({'task': task})
-
-    def post(self, request, pk):
-        task = get_object_or_404(Task, pk=pk)
-        task.delete()
-        return redirect('task_list')
+    def get_success_url(self):
+        next_url = self.request.POST.get('next', None)
+        if next_url:
+            return next_url
+        else:
+            return reverse_lazy('task_list')
 
 
 class TaskDeleteSelectedView(View):
@@ -77,3 +55,55 @@ class TaskDeleteSelectedView(View):
         selected_tasks = request.POST.getlist('selected_tasks')
         Task.objects.filter(pk__in=selected_tasks).delete()
         return redirect('task_list')
+
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'project/project_list.html'
+    context_object_name = 'projects'
+    ordering = 'start_date'
+    paginate_by = 5
+
+
+class ProjectDetailView(DetailView):
+    template_name = 'project/projetc_view.html'
+    model = Project
+    context_object_name = 'project'
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    form_class = ProjectForm
+    template_name = 'project/project_form.html'
+    success_url = reverse_lazy('project_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = self.form_class()
+        return context
+
+
+class ProjectUpdateView(UpdateView):
+    model = Project
+    form_class = ProjectForm
+    template_name = 'project/project_form.html'
+    success_url = reverse_lazy('project_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if 'form' not in context:
+            context['form'] = self.form_class(instance=self.object)
+        return context
+
+
+class ProjectDeleteView(DeleteView):
+    model = Project
+    template_name = 'project/project_confirm_delete.html'
+    success_url = reverse_lazy('project_list')
+
+
+class ProjectDeleteSelectedView(View):
+    def post(self, request):
+        selected_projects = request.POST.getlist('selected_projects')
+        Project.objects.filter(pk__in=selected_projects).delete()
+        return redirect('project_list')
